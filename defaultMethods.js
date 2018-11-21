76ad5bffe293a401d58323ce94ffc1b368b7019a
Andrew Beletskiy <andrewbeletskiy@gmail.com>
2018-11-21 16:34:48 +0200

    Made example method without sideeffect

--- defaultMethods.js
+++ defaultMethods.js
@@ -414,7 +414,12 @@ module.exports = () => ({
     if (!values.length) {
       throw new TypeError('There is not any example')
     }
+    const { explanation, [FIX_TREE]: fixTree } = this
+
+    this()
     values.forEach(this.throwError(schema, `Examples don't match the schema`))
+    this.explanation = explanation
+    this[FIX_TREE] = fixTree
     return this(schema)
   }
 })
